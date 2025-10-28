2c83dc8f587ee06fcaef1801ea33f8b708a7bd64
Harsh Peshwani <hpeshwani.dev@gmail.com>
2025-10-27 21:06:28 -0400

    Changes made in About Section page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -58,7 +58,7 @@
 >
 
 <Social class="flex flex-col w-4/5 md:max-w-2xl justify-center m-auto py-12 leading-relaxed space-y-3"/>
-	
+
 	<div
 		class="flex flex-col w-4/5 md:max-w-2xl justify-center m-auto leading-relaxed space-y-3"
 	>
